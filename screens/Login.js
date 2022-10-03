c683bdceac01688100b6a4ce599544529e35b6f2
Your Name <samadhinilakshana1999@gmail.com>
2022-10-03 11:09:28 +0530

    Update Content

--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
+import Background from './Background'
 
 export default function Login() {
   return (
-    <Text>
-      Login
-    </Text>
+    <Background>
+      <View style={{alignItems:"center",width:400}}>
+       <Text style={{color:"white",fontSize:64,fontWeight:'bold'}}>Login</Text>
+      </View>
+    </Background>
   )
 }
