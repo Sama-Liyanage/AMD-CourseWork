6d037ef528063ce2b4f560e74d9d3d3998f5c0e1
Your Name <samadhinilakshana1999@gmail.com>
2022-10-03 11:21:49 +0530

    Update Content

--- screens/Login.js
+++ screens/Login.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-import Background from './Background'
+import Background from './Background';
+import { darkGreen } from './Constants';
 
 export default function Login() {
   return (
     <Background>
-      <View style={{alignItems:"center",width:400}}>
-       <Text style={{color:"white",fontSize:64,fontWeight:'bold'}}>Login</Text>
+      <View style={{ alignItems: "center", width: 460 }}>
+        <Text style={{ color: "white", fontSize: 64, fontWeight: 'bold', marginVertical: 10 }}>Login</Text>
+        <View style={{ backgroundColor: "white", height: 700, width: 460, borderTopLeftRadius: 130, paddingTop: 100, alignItems: 'center' }}>
+          <Text style={{ fontSize: 40, color:darkGreen }}>Welcome Back</Text>
+        </View>
       </View>
     </Background>
   )
