8172da4463e2641e3c7575e534f528ac82a869a8
Your Name <samadhinilakshana1999@gmail.com>
2022-10-03 12:05:22 +0530

    Update Content

--- screens/Login.js
+++ screens/Login.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import Field from '../components/Field';
 import Background from './Background';
 import { darkGreen } from './Constants';
 
 export default function Login() {
   return (
     <Background>
-      <View style={{ alignItems: "center", width: 460 }}>
+      <View
+        style={{ alignItems: "center", width: 460 }}>
         <Text style={{ color: "white", fontSize: 64, fontWeight: 'bold', marginVertical: 10 }}>Login</Text>
         <View style={{ backgroundColor: "white", height: 700, width: 460, borderTopLeftRadius: 130, paddingTop: 100, alignItems: 'center' }}>
-          <Text style={{ fontSize: 40, color:darkGreen }}>Welcome Back</Text>
+          <Text style={{ fontSize: 40, color: darkGreen }}>Welcome Back</Text>
+          <Text style={{color:"gray",fontSize: 19,fontWeight:"bold",marginBottom: 20}}>Log in to your account</Text>
+          <Field
+            placeholder="Email / Username"
+            keyboardType={'email-address'}
+          />
+          <Field
+            placeholder="Password"
+            secureTextEntry={true}
+          />
+          <View
+            style={{alignItems: 'center', width: '78%'}}>
+            <Text style={{color: darkGreen, fontWeight: 'bold', fontSize: 16}}>
+              Forgot Password ?
+            </Text>
+          </View>
         </View>
       </View>
     </Background>
