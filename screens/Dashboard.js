668064a737e6e72af12c549350a396cf9ddd9ff4
Your Name <samadhinilakshana1999@gmail.com>
2022-10-05 20:15:07 +0530

    Update Content

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
 // Screens
 import VehicleDetails from './VehicleDetails';
 import WishList from './WishList';
+import AddVehicle from './AddVehicle'
 
 
 const Tab = createBottomTabNavigator();
@@ -17,14 +18,20 @@ export default function Dashboard() {
     <Tab.Navigator headerMode={'none'}>
       <Tab.Screen name="Wish List"
         options={{
-          tabBarIcon: () => (<Image source={require("../assets/icon/info.png")} style={{ width: 20, height: 20 }} />), header: () => null
-        }}
-        component={VehicleDetails} />
-      <Tab.Screen name="Vehicle Information"
-        options={{
           tabBarIcon: () => (<Image source={require("../assets/icon/love.png")} style={{ width: 20, height: 20 }} />), header: () => null
         }}
         component={WishList} />
+      <Tab.Screen name="Vehicle Information"
+        options={{
+          tabBarIcon: () => (<Image source={require("../assets/icon/info.png")} style={{ width: 20, height: 20 }} />), header: () => null
+        }}
+        component={VehicleDetails} />
+         <Tab.Screen name="Add Vehicle"
+        options={{
+          tabBarIcon: () => (<Image source={require("../assets/icon/plus-sign.png")} style={{ width: 20, height: 20 }} />), header: () => null
+        }}
+        component={AddVehicle} />
+
 
 
     </Tab.Navigator>
